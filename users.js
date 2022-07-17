23ade1df7e5ed3acdad49eb7f5024425798d9ff9
MarcoRamcharan <miguelmarcoramcharan@gmail.com>
2022-07-17 23:08:25 +0200

    added some more users

--- users.js
+++ users.js
@@ -3,36 +3,80 @@ module.exports = [
         id: 1,
         name: 'marco',
         surname: 'john',
-        age: 23
+        age: 23,
+        gender: 'male',
+        married: false
     },
     {
         id: 2,
-        name: 'lionel',
-        surname: 'messi',
-        age: 12
+        name: 'kelly ',
+        surname: 'rowland',
+        age: 40,
+        gender: 'female',
+        married: true
     },
     {
         id: 3,
-        name: 'manuel',
-        surname: 'debrah',
-        age: 17
+        name: 'mario',
+        surname: 'ruiz',
+        age: 50,
+        gender: 'male',
+        married: true
     },
     {
         id: 4,
-        name: 'mario',
-        surname: 'dentino',
-        age: 25
+        name: 'jenny',
+        surname: 'ortega',
+        age: 30,
+        gender: 'female',
+        married: false
     },
     {
         id: 5,
-        name: 'adrian',
-        surname: 'jaanties',
-        age: 23
+        name: 'spud',
+        surname: 'milton',
+        age: 16,
+        gender: 'male',
+        married: false
     },
     {
-        id: 6,
-        name: 'tree',
-        surname: 'bob',
-        age: 25
-    }
+        id: 34,
+        name: 'lionel',
+        surname: 'messi',
+        age: 35,
+        gender: 'male',
+        married: true
+    },
+    {
+        id: 17,
+        name: 'meghan',
+        surname: 'rapinoe',
+        age: 33,
+        gender: 'female',
+        married: false
+    },
+    {
+        id: 10,
+        name: 'kaia',
+        surname: 'gerber',
+        age: 20,
+        gender: 'female',
+        married: false
+    },
+    {
+        id: 8,
+        name: 'danny',
+        surname: 'devito',
+        age: 66,
+        gender: 'male',
+        married: true
+    },
+    {
+        id: 99,
+        name: 'alex',
+        surname: 'ferano',
+        age: 29,
+        gender: 'male',
+        married: true
+    },
 ]
